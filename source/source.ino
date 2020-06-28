d9088e8c1c63a3b07af2c805110445cad893bb88
Quint Guvernator <quint@guvernator.net>
2020-06-28 03:09:54 +0200

    simple ntp clock

--- source/source.ino
+++ source/source.ino
@@ -18,116 +18,90 @@
 #else
 #include <WiFi.h>
 #endif
+#include <NTPClient.h>
 #include <WiFiUdp.h>
 
 #include "VFD.h"
 
+const char* ssid = "ssid";
+const char* password = "password";
 
-const char* ssid = "SSID";
-const char* password = "PASS";
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
 
-const char* address = "revspace.nl";
-const int port = 64738;
+void show_time()
+{
+	while (!timeClient.update())
+		timeClient.forceUpdate();
 
-WiFiUDP udp;
-
-struct __attribute__ ((packed)) mumbleRequest {
-  unsigned int command;
-  unsigned long long ident;
-};
-
-byte reqBuffer[12];
-char respBuffer[24];
-
-
+	String t = timeClient.getFormattedTime().c_str();
+        char b[13] = "            ";
+        if (t[0] != '0') b[3] = t[0];
+        b[4] = t[1];
+        b[6] = t[3];
+        b[7] = t[4];
+        Vfd.write(b);
+}
 
 void setup()
 {
-  //pinMode(Pin_RED_LED13, OUTPUT);
-  //digitalWrite(Pin_RED_LED13, HIGH);
   pinMode(D3, OUTPUT);
   digitalWrite(D3, LOW);
-  Serial.begin(115200);
+
   Vfd.init();
-  Vfd.write("Connecting Wifi");
-  Serial.println("VFD Demo");
-  //WiFi.mode(WIFI_STA);
+  Vfd.write("            ");
+
   WiFi.begin(ssid, password);
-  Serial.println();
   while (WiFi.status() != WL_CONNECTED) {
-    delay(100);
-    Serial.print(".");
+    Vfd.write("| O         ");
+    delay(20);
+    Vfd.write("/  O        ");
+    delay(20);
+    Vfd.write("-   O       ");
+    delay(20);
+    Vfd.write("\\    O      ");
+    delay(20);
+    Vfd.write("|     O     ");
+    delay(20);
+    Vfd.write("/      O    ");
+    delay(20);
+    Vfd.write("-       O   ");
+    delay(20);
+    Vfd.write("\\        O  ");
+    delay(20);
+    Vfd.write("|         O ");
+    delay(20);
+    Vfd.write("/          O");
+    delay(20);
+    Vfd.write("-          O");
+    delay(20);
+    Vfd.write("\\         O ");
+    delay(20);
+    Vfd.write("|        O  ");
+    delay(20);
+    Vfd.write("/       O   ");
+    delay(20);
+    Vfd.write("-      O    ");
+    delay(20);
+    Vfd.write("\\     O     ");
+    delay(20);
+    Vfd.write("|    O      ");
+    delay(20);
+    Vfd.write("/   O       ");
+    delay(20);
+    Vfd.write("-  O        ");
+    delay(20);
+    Vfd.write("\\ O         ");
+    delay(20);
   }
-  Serial.println("Done connecting");
-  udp.begin(port);
 
+  timeClient.begin();
+  timeClient.setTimeOffset(7200);
 }
-
-int mompelaars = 99;
-int mompelaars_oud = 0;
 
 void loop()
 {
-  mompelaars_oud = mompelaars;
-  mompelaars = mumblers();
-  if (mompelaars_oud != mompelaars) {
-    fadeout();
-    char data[12];
-
-    if ((mompelaars == 0) || (mompelaars > 1)) {
-      sprintf(data, "%u mumblers  ", mompelaars);
-
-    } else {
-      sprintf(data, "%u mumbler   ", mompelaars);
-    }
-    Serial.println(data);
-    Vfd.write("            "); // clear screen ;)
-    Vfd.write(data);
-
-    Serial.println();
-    fadein();
-
-  } else {
-    //Serial.print("");
-    delay(100);
-  }
-}
-
-
-int mumblers() {
-  mumbleRequest request = {
-    .command = 0,
-    .ident = millis()
-  };
-
-  memcpy(reqBuffer, &request, sizeof(request));
-  udp.beginPacket(address, port);
-  udp.write(reqBuffer, 12);
-  udp.endPacket();
-
-  udp.parsePacket();
-  udp.read(respBuffer, 24);
-  // 4 bytes version
-  // 8 bytes ident
-  // 4 bytes connected
-  // 4 bytes maximum connections
-  // 4 bytes bandwidth
-  int connectedpeople = respBuffer[15];
-  return (connectedpeople);
-}
-
-void fadein()
-{
-  for (int i = 1; i <= 15; i++) {
-    Vfd.setDuty(i);
-    delay(80);
-  }
-}
-
-void fadeout()
-{
-  for (int i = 1; i <= 15; i++) {
-    Vfd.setDuty(16 - i);
-    delay(80);
-  }
+  Vfd.write("            ");
+  show_time();
+  delay(500);
 }
