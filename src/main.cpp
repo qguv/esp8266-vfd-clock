6b205e4753de3a6cc1561f52b163a48d58c0389d
Quint Guvernator <quint@guvernator.net>
2022-05-29 21:33:08 +0200

    use ESP-WiFiSettings

--- src/main.cpp
+++ src/main.cpp
@@ -13,25 +13,37 @@
 // D7   -  DIN
 
 #include <Arduino.h>
-#if defined(ARDUINO_ARCH_ESP8266)
-#include <ESP8266WiFi.h>
-#else
-#include <WiFi.h>
-#endif
+
+// portal & ota
+#include <LittleFS.h>
+#include <WiFiSettings.h>
+#include <ArduinoOTA.h>
+
+// ntp
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
-#include "VFD.h"
+#include <VFD.h>
 
-const char* ssid = "ssid";
-const char* password = "password";
-const long utcOffsetSec = 2 * 60 * 60;
-const long updateIntervalMs = 24 * 60 * 60 * 1000;
+#define ANIM_FRAME_MS 40
+#define WIFI_TIMEOUT_SEC 30
+#define ARRAY_LEN(X) (sizeof (X) / sizeof (X)[0])
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 
-void show_time()
+bool should_blink;
+int utc_offset_sec;
+long update_interval_ms;
+
+void setup_ota()
+{
+  ArduinoOTA.setHostname(WiFiSettings.hostname.c_str());
+  ArduinoOTA.setPassword(WiFiSettings.password.c_str());
+  ArduinoOTA.begin();
+}
+
+void show_time(bool blink)
 {
   timeClient.update();
 
@@ -41,76 +53,131 @@ void show_time()
   b[4] = t[1];
   b[6] = t[3];
   b[7] = t[4];
+  if (blink) {
+    b[5] = '.';
+  }
   Vfd.write(b);
 }
 
+char * const anim_connecting[24] = {
+    "->          ",
+    "-->         ",
+    "--->        ",
+    " --->       ",
+    "  --->      ",
+    "   --->     ",
+    "    --->    ",
+    "     --->   ",
+    "      --->  ",
+    "       ---> ",
+    "        --->",
+    "         --<",
+    "          <-",
+    "         <--",
+    "        <---",
+    "       <--- ",
+    "      <---  ",
+    "     <---   ",
+    "    <---    ",
+    "   <---     ",
+    "  <---      ",
+    " <---       ",
+    "<---        ",
+    ">--         ",
+};
+
+char * const anim_config[4] = {
+    " - config | ",
+    " \\ config / ",
+    " | config - ",
+    " / config \\ ",
+};
+
 void setup()
 {
+  Serial.begin(115200);
+  LittleFS.begin();
+
   pinMode(D3, OUTPUT);
   digitalWrite(D3, LOW);
 
   Vfd.init();
   Vfd.write("            ");
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    Vfd.write("->          ");
-    delay(40);
-    Vfd.write("-->         ");
-    delay(40);
-    Vfd.write("--->        ");
-    delay(40);
-    Vfd.write(" --->       ");
-    delay(40);
-    Vfd.write("  --->      ");
-    delay(40);
-    Vfd.write("   --->     ");
-    delay(40);
-    Vfd.write("    --->    ");
-    delay(40);
-    Vfd.write("     --->   ");
-    delay(40);
-    Vfd.write("      --->  ");
-    delay(40);
-    Vfd.write("       ---> ");
-    delay(40);
-    Vfd.write("        --->");
-    delay(40);
-    Vfd.write("         --<");
-    delay(40);
-    Vfd.write("          <-");
-    delay(40);
-    Vfd.write("         <--");
-    delay(40);
-    Vfd.write("        <---");
-    delay(40);
-    Vfd.write("       <--- ");
-    delay(40);
-    Vfd.write("      <---  ");
-    delay(40);
-    Vfd.write("     <---   ");
-    delay(40);
-    Vfd.write("    <---    ");
-    delay(40);
-    Vfd.write("   <---     ");
-    delay(40);
-    Vfd.write("  <---      ");
-    delay(40);
-    Vfd.write(" <---       ");
-    delay(40);
-    Vfd.write("<---        ");
-    delay(40);
-    Vfd.write(">--         ");
-    delay(40);
-  }
+  should_blink = WiFiSettings.checkbox("should_blink", false, "Should the timer blink?");
+  utc_offset_sec = WiFiSettings.integer("utc_offset_sec", 1 * 60 * 60, "UTC offset (seconds)");
+  update_interval_ms = 1000L * (long) WiFiSettings.integer("update_interval_sec", 24 * 60 * 60, "NTP update interval (seconds)");
 
-  timeClient.begin();
-  timeClient.setTimeOffset(utcOffsetSec);
-  timeClient.setUpdateInterval(updateIntervalMs);
+  WiFiSettings.onWaitLoop = []() {
+    static unsigned long last_frame = ARRAY_LEN(anim_connecting);
+    unsigned long frame = millis() / ANIM_FRAME_MS % ARRAY_LEN(anim_connecting);
+    if (last_frame != frame) {
+      last_frame = frame;
+      Vfd.write(anim_connecting[frame]);
+    }
+    return 50;
+  };
+
+  WiFiSettings.onSuccess = []() {
+    Vfd.write("    ota  ntp");
+    delay(ANIM_FRAME_MS);
+    Vfd.write(">   ota  ntp");
+    delay(ANIM_FRAME_MS);
+    Vfd.write("->  ota  ntp");
+    delay(ANIM_FRAME_MS);
+    Vfd.write("--> ota  ntp");
+    setup_ota();
+
+    Vfd.write("--->ota  ntp");
+    delay(ANIM_FRAME_MS);
+    Vfd.write("---->ta  ntp");
+    delay(ANIM_FRAME_MS);
+    Vfd.write("----->a  ntp");
+    delay(ANIM_FRAME_MS);
+    Vfd.write("------>  ntp");
+    delay(ANIM_FRAME_MS);
+
+    Vfd.write("-------> ntp");
+    timeClient.begin();
+    timeClient.setTimeOffset(utc_offset_sec);
+    timeClient.setUpdateInterval(update_interval_ms);
+
+    Vfd.write("  ------>ntp");
+    delay(ANIM_FRAME_MS);
+    Vfd.write("    ----->tp");
+    delay(ANIM_FRAME_MS);
+    Vfd.write("      ---->p");
+    delay(ANIM_FRAME_MS);
+    Vfd.write("        --->");
+    delay(ANIM_FRAME_MS);
+    Vfd.write("          --");
+    delay(ANIM_FRAME_MS);
+    Vfd.write("            ");
+  };
+
+  WiFiSettings.onPortal = []() {
+    setup_ota();
+  };
+
+  WiFiSettings.onPortalWaitLoop = []() {
+    static unsigned long last_frame = ARRAY_LEN(anim_connecting);
+    unsigned long frame = millis() / ANIM_FRAME_MS % ARRAY_LEN(anim_connecting);
+    if (last_frame != frame) {
+      last_frame = frame;
+      Vfd.write(anim_config[frame]);
+    }
+    ArduinoOTA.handle();
+  };
+
+  WiFiSettings.connect(true, WIFI_TIMEOUT_SEC);
 }
 
 void loop()
 {
-  show_time();
+  static bool blinking = false;
+  show_time(blinking);
+  if (should_blink) {
+    blinking = !blinking;
+  }
   delay(1000);
 }
